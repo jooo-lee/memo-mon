d26057d8c8d4a4859ce02308e7b4f3dfaa13598e
Joseph Lee <josephlee3004@gmail.com>
2024-07-18 16:53:34 -0400

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h1>MemoMon</h1>
+      <p>
+        Get points by clicking on an image but don&apos;t click on any more than
+        once!
+      </p>
+    </header>
+  );
+}
+
+export default Header;
