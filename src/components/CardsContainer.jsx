63f3a51c28112ec668214c4c98015f9277241860
Joseph Lee <josephlee3004@gmail.com>
2024-07-21 14:34:11 -0400

    Randomize card order on mount and card click

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -4,15 +4,37 @@ import { useEffect, useState } from 'react';
 // Instantiate the SDK
 const tcgdex = new TCGdex('en');
 
+/* 
+Fisher-Yates (aka Knuth) shuffle from:
+https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+*/
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
+
 function CardsContainer() {
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
+    const pokemonIds = [44, 45, 48, 50, 51, 52, 53, 56, 59, 60, 64, 68];
     (async () => {
-      const pokemonIds = [45, 50, 51, 52, 56, 59, 60, 64, 68];
       const response = await Promise.all(
         pokemonIds.map((id) => tcgdex.fetch('cards', `ex16-${id}`))
       );
+      shuffle(response);
       setCards(response);
     })();
   }, []);
@@ -21,15 +43,19 @@ function CardsContainer() {
     <ul
       style={{
         listStyle: 'none',
-        display: 'grid',
-        gridTemplateColumns: '200px 200px 200px',
-        columnGap: '1.5rem',
-        rowGap: '1rem',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
       }}>
       {cards === null ? 'Fetching card data...' : ''}
       {cards.map((pokemon) => (
         <li key={pokemon.id}>
-          <button>
+          <button
+            onClick={() => {
+              const nextCards = [...cards];
+              shuffle(nextCards);
+              setCards(nextCards);
+            }}>
             <img
               src={`${pokemon.image}/high.webp`}
               alt=""
