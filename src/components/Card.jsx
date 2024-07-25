21157a8f606c523fcb7a571d7ca6880268a36851
Joseph Lee <josephlee3004@gmail.com>
2024-07-25 14:49:27 -0400

    Add tilt effect to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Tilt from 'react-parallax-tilt';
 
 import '../styles/Card.css';
 
@@ -6,19 +7,21 @@ function Card({ pokémon, setScore, setIsLoss }) {
   const [clicked, setClicked] = useState(false);
 
   return (
-    <li className="card">
-      <button
-        onClick={() => {
-          if (clicked) {
-            setIsLoss(true);
-          } else {
-            setClicked(true);
-            setScore((prevScore) => prevScore + 1);
-          }
-        }}>
-        <img src={pokémon.imgUrl} alt={pokémon.name} />
-      </button>
-    </li>
+    <Tilt glareEnable={true} glareMaxOpacity={0.45} glarePosition="all">
+      <li className="card">
+        <button
+          onClick={() => {
+            if (clicked) {
+              setIsLoss(true);
+            } else {
+              setClicked(true);
+              setScore((prevScore) => prevScore + 1);
+            }
+          }}>
+          <img src={pokémon.imgUrl} alt={pokémon.name} />
+        </button>
+      </li>
+    </Tilt>
   );
 }
 
