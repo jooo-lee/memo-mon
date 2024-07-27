b34f781b3422e8dc73b12e80b4e03b2947734d91
Joseph Lee <josephlee3004@gmail.com>
2024-07-27 16:34:15 -0400

    Change verb tense

--- src/components/WinDialog.jsx
+++ src/components/WinDialog.jsx
@@ -3,7 +3,7 @@ import '../styles/WinDialog.css';
 function WinDialog({ setScore }) {
   return (
     <div className="win-dialog">
-      <p>You win! &#127881;</p>
+      <p>You won! &#127881;</p>
       <button
         onClick={() => {
           setScore(0);
